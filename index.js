f46e0ed8eb7b01d5f939ecd80b62f823885ed7d0
Weigel <jesse.weigel@dcsg.com>
2020-04-07 16:08:33 -0400

    transpile index.js to es5 syntax

--- index.js
+++ index.js
@@ -1,39 +1,133 @@
-const nativeBigInt = (typeof global !== 'undefined') && global.BigInt || (typeof window !== 'undefined') && window.BigInt;
-const supportsNative = typeof nativeBigInt === 'function';
-const _BigInt = require('big-integer');
+"use strict";
 
-const nativeFunction = function (operator, a) {
-    switch (operator) {
-      // Arithmetic operators
-      case 'add': return (b) => new BigInt(a + b);
-      case 'subtract': return (b) => new BigInt(a - b);
-      case 'multiply': return (b) => new BigInt(a * b);
-      case 'divide': return (b) => new BigInt(a / b);
-      case 'remainder': return (b) => new BigInt(a % b);
-      case 'pow': return (b) => new BigInt(a ** b);
-      // Bitwise shift operators
-      case 'shiftLeft': return (b) => new BigInt(a << b);
-      case 'shiftRight': return (b) => new BigInt(a >> b);
-      // Binary bitwise operators
-      case 'and': return (b) => new BigInt(a & b);
-      case 'or': return (b) => new BigInt(a | b);
-      case 'xor': return (b) => new BigInt(a ^ b);
-      // Relational operators
-      case 'lt': return (b) => a < b;
-      case 'gt': return (b) => a > b;
-      case 'leq': return (b) => a <= b;
-      case 'geq': return (b) => a >= b;
-      case 'eq': return (b) => a === b;
-      case 'neq': return (b) => a !== b;
-      // Unary operators
-      case 'negate': return () => new BigInt(- a);
-      case 'not': return () => new BigInt(~ a);
-      case 'next': return () => new BigInt(++a);
-      case 'prev': return () => new BigInt(--a);
-    }
-    return undefined;
+function _instanceof(left, right) { if (right != null && typeof Symbol !== "undefined" && right[Symbol.hasInstance]) { return !!right[Symbol.hasInstance](left); } else { return left instanceof right; } }
+
+function _classCallCheck(instance, Constructor) { if (!_instanceof(instance, Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var nativeBigInt = typeof global !== 'undefined' && global.BigInt || typeof window !== 'undefined' && window.BigInt;
+var supportsNative = typeof nativeBigInt === 'function';
+
+var _BigInt = require('big-integer');
+
+var nativeFunction = function nativeFunction(operator, a) {
+  switch (operator) {
+    // Arithmetic operators
+    case 'add':
+      return function (b) {
+        return new BigInt(a + b);
+      };
+
+    case 'subtract':
+      return function (b) {
+        return new BigInt(a - b);
+      };
+
+    case 'multiply':
+      return function (b) {
+        return new BigInt(a * b);
+      };
+
+    case 'divide':
+      return function (b) {
+        return new BigInt(a / b);
+      };
+
+    case 'remainder':
+      return function (b) {
+        return new BigInt(a % b);
+      };
+
+    case 'pow':
+      return function (b) {
+        return new BigInt(a ** b);
+      };
+    // Bitwise shift operators
+
+    case 'shiftLeft':
+      return function (b) {
+        return new BigInt(a << b);
+      };
+
+    case 'shiftRight':
+      return function (b) {
+        return new BigInt(a >> b);
+      };
+    // Binary bitwise operators
+
+    case 'and':
+      return function (b) {
+        return new BigInt(a & b);
+      };
+
+    case 'or':
+      return function (b) {
+        return new BigInt(a | b);
+      };
+
+    case 'xor':
+      return function (b) {
+        return new BigInt(a ^ b);
+      };
+    // Relational operators
+
+    case 'lt':
+      return function (b) {
+        return a < b;
+      };
+
+    case 'gt':
+      return function (b) {
+        return a > b;
+      };
+
+    case 'leq':
+      return function (b) {
+        return a <= b;
+      };
+
+    case 'geq':
+      return function (b) {
+        return a >= b;
+      };
+
+    case 'eq':
+      return function (b) {
+        return a === b;
+      };
+
+    case 'neq':
+      return function (b) {
+        return a !== b;
+      };
+    // Unary operators
+
+    case 'negate':
+      return function () {
+        return new BigInt(-a);
+      };
+
+    case 'not':
+      return function () {
+        return new BigInt(~a);
+      };
+
+    case 'next':
+      return function () {
+        return new BigInt(++a);
+      };
+
+    case 'prev':
+      return function () {
+        return new BigInt(--a);
+      };
+  }
+
+  return undefined;
 };
-
 /**
  * A proxy for BigInt that supports both native and polyfill implementations.
  * Also includes convenience functions for conversion to and from uint8 arrays
@@ -46,76 +140,93 @@ const nativeFunction = function (operator, a) {
  *     const biNonNative = new BigInt('9223372036854775807');
  *     biNonNative.toUint8Array()
  */
-class BigInt {
-    constructor(value) {
-        if (typeof value === 'bigint') {
-            this.value = value;
-        } else if (value instanceof BigInt) {
-            return value; 
+
+
+var BigInt = /*#__PURE__*/function () {
+  function BigInt(value) {
+    _classCallCheck(this, BigInt);
+
+    if (typeof value === 'bigint') {
+      this.value = value;
+    } else if (_instanceof(value, BigInt)) {
+      return value;
+    } else {
+      this.value = new _BigInt(value);
+    } // Proxy method calls to _BigInt if possible
+
+
+    return new Proxy(this, {
+      get: function get(obj, field) {
+        if (field in obj) return obj[field];
+        if (typeof obj.value === 'bigint') return nativeFunction(field, obj.value);
+        if (typeof obj.value !== 'bigint' && field in obj.value) return obj.value[field].bind(obj.value);
+        return undefined;
+      }
+    });
+  }
+
+  _createClass(BigInt, [{
+    key: "valueOf",
+    value: function valueOf() {
+      if (typeof this.value === 'bigint') {
+        return this.value;
+      } else {
+        throw new Error('Cannot implicitly cast polyfilled BigInt into number');
+      }
+    }
+  }, {
+    key: "equals",
+    value: function equals(b) {
+      if (typeof this.value === 'bigint') {
+        return this.value === b.value;
+      } else if (_instanceof(b, BigInt)) {
+        if (typeof b.value === 'bigint') {
+          return this.value.equals(new _BigInt(b.toString()));
         } else {
-            this.value = new _BigInt(value);
+          return this.value.equals(new _BigInt(b.value));
         }
-        // Proxy method calls to _BigInt if possible
-        return new Proxy(this, {
-            get(obj, field) {
-                if (field in obj) return obj[field];
-                if (typeof obj.value === 'bigint') return nativeFunction(field, obj.value);
-                if (typeof obj.value !== 'bigint' && field in obj.value) return obj.value[field].bind(obj.value);
-                return undefined;
-            }
-        });
+      } else {
+        return this.value.equals(new _BigInt(b));
+      }
     }
-
-    valueOf() {
-        if (typeof this.value === 'bigint') {
-            return this.value;
-        } else {
-            throw new Error('Cannot implicitly cast polyfilled BigInt into number')
-        }
+  }, {
+    key: "toString",
+    value: function toString() {
+      return this.value.toString();
     }
-
-    equals(b) {
-        if (typeof this.value === 'bigint') {
-            return this.value === b.value;
-        } else if (b instanceof BigInt) {
-            if (typeof b.value === 'bigint') {
-                return this.value.equals(new _BigInt(b.toString()));
-            } else {
-                return this.value.equals(new _BigInt(b.value));
-            } 
-        } else {
-            return this.value.equals(new _BigInt(b));
-        }
+  }, {
+    key: "_toUint8ArrayNative",
+    value: function _toUint8ArrayNative() {
+      var littleEndian = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
+      var elements = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 8;
+      var arr = new ArrayBuffer(elements);
+      var view = new DataView(arr);
+      view.setBigUint64(0, this.value, littleEndian);
+      return new Uint8Array(arr);
     }
-
-    toString() {
-        return this.value.toString();
+  }, {
+    key: "_toUint8Array",
+    value: function _toUint8Array() {
+      var littleEndian = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
+      var elements = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 8;
+      var arr = new ArrayBuffer(elements);
+      var uint8arr = new Uint8Array(arr);
+      var intarr = this.value.toArray(2 ** 8).value;
+      if (littleEndian) uint8arr.set(intarr.reverse(), 0);else uint8arr.set(intarr, elements - intarr.length);
+      return uint8arr;
     }
+  }, {
+    key: "toUint8Array",
+    value: function toUint8Array() {
+      var littleEndian = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : false;
+      var elements = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 8;
 
-    _toUint8ArrayNative(littleEndian = false, elements = 8) {
-        const arr = new ArrayBuffer(elements);
-        const view = new DataView(arr);
-        view.setBigUint64(0, this.value, littleEndian);
-        return new Uint8Array(arr);
+      if (typeof this.value === 'bigint') {
+        return this._toUint8ArrayNative(littleEndian, elements);
+      } else {
+        return this._toUint8Array(littleEndian, elements);
+      }
     }
-
-    _toUint8Array(littleEndian = false, elements = 8) {
-        const arr = new ArrayBuffer(elements);
-        const uint8arr = new Uint8Array(arr);
-        const intarr = this.value.toArray(2**8).value;
-        if (littleEndian) uint8arr.set(intarr.reverse(), 0);
-        else uint8arr.set(intarr, elements - intarr.length);
-        return uint8arr;
-    }
-
-    toUint8Array(littleEndian = false, elements = 8) {
-        if (typeof this.value === 'bigint') {
-            return this._toUint8ArrayNative(littleEndian, elements);
-        } else {
-            return this._toUint8Array(littleEndian, elements);
-        }
-    }
-
     /**
      * Get BigInt from a uint8 array in specified endianess.
      * Uses native BigInt if the environment supports it and detectSupport is true.
@@ -124,19 +235,31 @@ class BigInt {
      * @param {boolean} littleEndian use little endian byte order, default is false
      * @param {boolean} detectSupport auto-detect support for native BigInt, default is true
      */
-    static fromUint8Array(uint8arr, littleEndian = false, detectSupport = true) {
-        if (supportsNative && detectSupport) {
-            const view = new DataView(uint8arr.buffer);
-            return new BigInt(view.getBigUint64(0, littleEndian));
-        }
-        let array;
-        if (littleEndian) {
-            array = Array.from(uint8arr).reverse();
-        } else {
-            array = Array.from(uint8arr);
-        }
-        return new BigInt(_BigInt.fromArray(array, 2**8));
+
+  }], [{
+    key: "fromUint8Array",
+    value: function fromUint8Array(uint8arr) {
+      var littleEndian = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false;
+      var detectSupport = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : true;
+
+      if (supportsNative && detectSupport) {
+        var view = new DataView(uint8arr.buffer);
+        return new BigInt(view.getBigUint64(0, littleEndian));
+      }
+
+      var array;
+
+      if (littleEndian) {
+        array = Array.from(uint8arr).reverse();
+      } else {
+        array = Array.from(uint8arr);
+      }
+
+      return new BigInt(_BigInt.fromArray(array, 2 ** 8));
     }
-}
+  }]);
+
+  return BigInt;
+}();
 
 module.exports = BigInt;
